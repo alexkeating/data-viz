d8cdff14ac61da17a13f8e827d0f0351529eb5c4
Alexander Keating <alex@wayup.com>
2016-11-28 20:35:08 -0500

    Added routing
    Added react router and routing for the editor component.

--- frontend-dev/src/index.js
+++ frontend-dev/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './sql-editor/components/App';
+import { render } from 'react-dom';
+import {  BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import EditorApp from './sql-editor/components/EditorApp';
+import NotFound from './sql-editor/components/NotFound';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={EditorApp} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(
+    <Root/>,
+    document.getElementById('root')
 );
