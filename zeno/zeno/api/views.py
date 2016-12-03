09066307362885c23740bd949444d254600feb49
Alexander Keating <alex@wayup.com>
2016-12-03 16:54:05 -0500

    Added query endpoint
    Now you can get and post queries from the database. If you don't specify an endpoint you will get all queries. This is so users can create and eventually display queries.

--- zeno/zeno/api/views.py
+++ zeno/zeno/api/views.py
@@ -1,5 +1,5 @@
 import os
-from mainsite.models import Dashboard
+from mainsite.models import Dashboard, Query
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -50,9 +50,56 @@ class DashboardViewSet(APIView):
         dashboard_dict = request.data
         try:
             dashboard = Dashboard.objects.get(id=dashboard_dict['id'])
-            dashboard.title =dashboard_dict['name']
+            dashboard.title = dashboard_dict['name']
             dashboard.save(update_fields=['title'])
         except Dashboard.DoesNotExist:
             Dashboard.objects.create(id=dashboard_dict['id'], title=dashboard_dict['name'])
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class QueryViewSet(APIView):
+    """
+    Queries your database
+    """
+
+    def get(self, request, **kwargs):
+        # queryset = Query.objects.filter(dashboard=request)
+        dashboard_id = kwargs.get('dashboard_id')
+        query_id = kwargs.get('query_id')
+        if not query_id:
+            try:
+                all_queries = Query.objects.get(dashboard=dashboard_id)
+            except Query.DoesNotExist:
+                response = {'queries': {}}
+                return Response(data=response, status=status.HTTP_201_CREATED)
+            response = {query.id: {'x': query.x, 'y': query.y, 'querystring': query.querystring,
+                                   'dashboard': query.dashboard} for query in all_queries}
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        query = Query.objects.get(id=query_id, dashboard=dashboard_id)
+
+        response = {'query': {'id': query.id, 'x': query.x, 'y': query.y, 'querystring': query.querystring,
+                              'dashboard': query.dashboard}}
+        return Response(data=response, status=status.HTTP_201_CREATED)
+
+    def post(self, request):
+        """
+        This will take a dashboardId and create a new dashboard object
+        :param request:
+        :return:
+        """
+
+        query_dict = request.data
+        try:
+            query = Dashboard.objects.get(id=query_dict['query_id'])
+            query.x = query_dict['x']
+            query.y = query_dict['y']
+            query.querystring = query_dict['querystring']
+            query.dashboard = query_dict['dashbaord_id']
+            query.save(update_fields=['x', 'y', 'querystring', 'dashboard_id'])
+        except Query.DoesNotExist:
+            Dashboard.objects.create(x=query_dict['x'], y=query_dict['y'],
+                                     querystring=query_dict['querystring'], dashboard=query_dict['dashboard'])
 
         return Response(status=status.HTTP_201_CREATED)
--- zeno/zeno/urls.py
+++ zeno/zeno/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/querystring', views.RunQueryViewSet.as_view()),
-    url(r'^api/v1/dashboard', views.DashboardViewSet.as_view()),
+    url(r'^api/v1/querystring/$', views.RunQueryViewSet.as_view()),
+    url(r'^api/v1/dashboard/$', views.DashboardViewSet.as_view()),
+url(r'^api/v1/dashboard/(?P<dashboard_id>\w+)/query/$', views.QueryViewSet.as_view()),
+    url(r'^api/v1/dashboard/(?P<dashboard_id>\w+)/query/(?P<query_id>\w+)$', views.QueryViewSet.as_view()),
 ]
