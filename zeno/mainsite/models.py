b3ef39112f5382061337fb60bec6b6c319b1da12
Alexander Keating <alex@wayup.com>
2016-12-05 21:08:46 -0500

    Added a chart type column

--- zeno/mainsite/models.py
+++ zeno/mainsite/models.py
@@ -9,9 +9,15 @@ class Dashboard(models.Model):
 
 
 class Query(models.Model):
+    CHART_CHOICES = ((0, 'Table'),
+                     (1, 'Line'),
+                     (2, 'Bar'))
 
     x_axis = models.CharField(max_length=100, blank=True)
     y_axis = models.CharField(max_length=100, blank=True)
+    chart_type = models.IntegerField(choices=CHART_CHOICES, default=0)
     querystring = models.TextField(blank=True)
     dashboard = models.ForeignKey('Dashboard', blank=True, null=True, related_name='query_set')
+
+
 
