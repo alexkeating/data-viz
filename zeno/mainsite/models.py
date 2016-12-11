3e1ea022d3a732bae825c52e0c0e966bdadfd9b5
Alexander Keating <alex@wayup.com>
2016-12-10 20:10:29 -0500

    Added database table
    Now a user can add multiple databases and connect them to queries.

--- zeno/mainsite/models.py
+++ zeno/mainsite/models.py
@@ -3,6 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Database(models.Model):
+
+    display_name = models.CharField(max_length=100, blank=True)
+    database_type = models.CharField(max_length=100)
+    database_name = models.CharField(max_length=100, blank=True)
+    database_host = models.CharField(max_length=100, blank=True)
+    database_port = models.IntegerField(blank=True, null=True)
+    database_username = models.CharField(max_length=100, blank=True)
+    database_password = models.CharField(max_length=100, blank=True)
+
+
 class Dashboard(models.Model):
 
     title = models.CharField(max_length=100, blank=True)
@@ -17,6 +28,7 @@ class Query(models.Model):
     y_axis = models.CharField(max_length=100, blank=True)
     chart_type = models.IntegerField(choices=CHART_CHOICES, default=0)
     querystring = models.TextField(blank=True)
+    database = models.ForeignKey('Database', blank=True, null=True)
     dashboard = models.ForeignKey('Dashboard', blank=True, null=True, related_name='query_set')
 
 
